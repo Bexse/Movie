9d82c00992dba24854a53e1d5cd828cfc98462ce
Bexse <89146106+Bexse@users.noreply.github.com>
2022-04-06 09:43:44 -0400

    Update MovieList.js

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react'
-import Movie from './Movie';
+import React, { Component } from "react";
+import Movie from "./Movie";
 
-export default class MovieList extends Component {
+export default class MovieLists extends Component {
   render() {
-      const movieLists  = this.props.movies;
-       
+   
 
     return (
       <div>
-
-          {
-          movieLists.map(movie => <Movie movie={movie} key={movie.id} addToFavorite={this.props.addToFavorite}/>)
-          }
-       
+            
+        MovieLists components
+        {this.props.movies
+        .filter(movie => movie.isDeleted === 0)
+        .map(movie => (
+          <Movie
+            movie={movie}
+           
+            key={movie.id}
+            updateMovie={this.props.updateMovie}
+            deleteMovie={this.props.deleteMovie}
+            addtoFavorite={this.props.addtoFavorite}
+          />
+        ))}
+ 
+        {this.props.favorites.map(fav=>(fav.id))}
       </div>
-    )
+    );
   }
 }
 
