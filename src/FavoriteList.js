4f4bb53ce0c9451137b6a9ec1782e9d731031290
Bexse <89146106+Bexse@users.noreply.github.com>
2022-04-06 09:46:55 -0400

    Update FavoriteList.js

--- src/FavoriteList.js
+++ src/FavoriteList.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-export default class FavoriteList extends Component {
-
-  state = {
-    favorites:[]
-  }
+export default class FavoriteMovies extends Component {
   render() {
-    
     return (
       <div>
-       List of Favorite Movies
-       <button> Display Favorite Movies</button>
+        FavoriteMovies will be displayed here
+        {this.props.favorites.id}
+        {/* {this.props.favorites
+          .filter(fav => fav.isDeleted === 0)
+          .map(fav => (
+            <div>
+              <p>
+                <span>{this.props.fav.id} </span>
+                <span>{this.props.fav.name} </span>
+                <span> {this.props.fav.genres} </span>
+                <span> {this.props.fav.rating} </span>
+              </p>
+            </div>
+          ))
+          
+          } */}
       </div>
     );
   }
