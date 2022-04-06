b75acd4ffc8f8eac7bb12ab02263455361fec3b1
Bexse <89146106+Bexse@users.noreply.github.com>
2022-04-06 09:43:59 -0400

    Update MovieList.js

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,31 +1,39 @@
 import React, { Component } from "react";
-import Movie from "./Movie";
+import UpdateMovie from "./UpdateMovie";
+import DeleteMovie from "./DeleteMovie";
+import DetailsButton from "./DetailsButton";
+import AddtoFavorite from "./AddtoFavorite";
 
-export default class MovieLists extends Component {
+export default class Movie extends Component {
   render() {
-   
-
     return (
       <div>
-            
-        MovieLists components
-        {this.props.movies
-        .filter(movie => movie.isDeleted === 0)
-        .map(movie => (
-          <Movie
-            movie={movie}
-           
-            key={movie.id}
-            updateMovie={this.props.updateMovie}
-            deleteMovie={this.props.deleteMovie}
-            addtoFavorite={this.props.addtoFavorite}
-          />
-        ))}
- 
-        {this.props.favorites.map(fav=>(fav.id))}
+        Movie component
+        <div>
+          <p>
+            <span>{this.props.movie.id} </span>
+            <span>{this.props.movie.name} </span>
+            <span> {this.props.movie.genres} </span>
+            <span> {this.props.movie.rating} </span>
+            {/* <span> {this.props.movie.directorName} </span>
+            <span> {this.props.movie.releaseYear} </span>
+            <span> {this.props.movie.description} </span> */}
+          </p>
+        </div>
+        <DetailsButton
+          id={this.props.movie.id}
+          movieDetail={this.props.movie}
+        />
+        <UpdateMovie
+          updateMovie={this.props.updateMovie}
+          id={this.props.movie.id}
+        />
+        <DeleteMovie
+          deleteMovie={this.props.deleteMovie}
+          id={this.props.movie.id}
+        />
+        <AddtoFavorite addtoFavorite={this.props.addtoFavorite} id={this.props.movie.id}/>
       </div>
     );
   }
 }
-
-
